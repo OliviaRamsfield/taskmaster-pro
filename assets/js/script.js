272f0399d9533a05d475afb9fadfb941327b2d46
Olivia <ramsfol@gmail.com>
2022-06-22 10:35:08 -0500

    droppable delete function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,6 +213,22 @@ $(".card .list-group").sortable({
   }
 });
 
+//drag and drop into remove task div
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
